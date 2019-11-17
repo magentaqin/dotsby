b7bafa4c87e26f980b251a21af2ad6f263310004
Mu Qin <magenta2127@gmail.com>
2019-11-18 07:06:56 +0800

    update api.raml example

--- src/lib/raml.js
+++ src/lib/raml.js
@@ -6,5 +6,5 @@ const ramlFilePath = path.resolve(__dirname, '../../docs/api.raml')
 
 ramlParser.parse(ramlFilePath).then(resp => {
   const ramlPages = ramlFormatter.getPages(resp)
-  console.log(ramlPages)
+  console.log(JSON.stringify(ramlPages))
 }).catch(err => console.log('RAML ERR', err))

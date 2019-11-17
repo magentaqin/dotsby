238bf87dbe7b01e37ad9176e24e01c9522e05ab4
Mu Qin <magenta2127@gmail.com>
2019-11-17 21:08:27 +0800

    parse raml file

---
+++ src/lib/parse/ramlFormatter.js
@@ -0,0 +1,98 @@
+const toSchema = (o) => {
+  if (o.properties) {
+    let obj = {
+      type: 'object',
+      description: o.description || '',
+      required: [],
+      properties: {}
+    }
+    o.properties.forEach((p) => {
+      obj.properties[p.key] = toSchema(p)
+    })
+    obj.required = Object.keys(obj.properties)
+    return obj;
+  } if (o.items) {
+    let obj = {
+      type: 'array',
+      description: o.description || '',
+      items: {}
+    }
+    if (o.items.content) {
+      obj.items = JSON.parse(o.items.content)
+    } else if (o.items.type && o.items.type.indexOf('$schema') > -1) {
+      obj.items = JSON.parse(o.items.type)
+    } else {
+      obj.items = toSchema(o.items)
+    }
+    return obj;
+  } if (o.content) {
+    return JSON.parse(o.content)
+  } if (o.type.indexOf('$schema') > -1) {
+    return JSON.parse(o.type)
+  }
+  let obj = { type: o.type, description: o.description || ''};
+  ['enum', 'example', 'examples'].forEach((property) => {
+    if (o.hasOwnProperty(property)) {
+      if (property === 'examples' && o[property].length === 1) {
+        obj['example'] = o[property][0].structuredValue
+      } else {
+        obj[property] = o[property];
+      }
+    }
+  })
+  return obj;
+}
+/**
+ * process through parsed raml object to get standard page array
+ *
+ * Page: {
+ *   name,
+ *   displayName,
+ *   resource,
+ * }
+ * @return {Array}
+ */
+const getPages = (obj) => {
+  const pages = [];
+  // convert raml resource (one url one resource)
+  // to page.resource (one url + http method one page.resource)
+  if (!obj.resources) {
+    return [];
+  }
+  obj.resources.forEach((res) => {
+    res.methods.forEach((mes) => {
+      // const uniqueId = res.uniqueId;
+      // const name = uniqueId ? mes.method + '_' + uniqueId : mes.method + '_root';
+      // Currently we don't use one uri for mutiple methods, so just use the uri as name
+      let name = res.relativeUri.slice(1);
+      if (name === '') {
+        name = 'root';
+      }
+
+      mes.relativeUri = res.relativeUri;
+      const displayName = mes.displayName ? mes.displayName : `${mes.method.toUpperCase()} ${mes.relativeUri}`;
+      mes.responses.forEach((child, cindex) => {
+        if (child.body) {
+          child.body.forEach((item, tindex) => {
+            try {
+              const schema = toSchema(item);
+              item.jsonschema = JSON.stringify(schema);
+            } catch (e) {
+              console.error('convert error: ', e)
+            }
+          })
+        }
+      })
+      pages.push({
+        name,
+        displayName,
+        resource: mes,
+      });
+    });
+  });
+  return pages;
+}
+
+module.exports = {
+  getPages,
+};
---
+++ src/lib/parse/ramlParser.js
@@ -0,0 +1,132 @@
+import raml from 'raml-1-parser';
+import tools from 'datatype-expansion';
+import fs from 'fs';
+import { ramlArrayToObject, recursiveObjectToArray, makeConsistent } from '@src/utils/raml';
+
+
+function _makeUniqueId(string) {
+  const stringWithSpacesReplaced = string.replace(/\W/g, '_');
+  const stringWithLeadingUnderscoreRemoved = stringWithSpacesReplaced.replace(new RegExp('^_+'), '');
+  return stringWithLeadingUnderscoreRemoved.toLowerCase();
+}
+
+// Add unique id's and parent URL's plus parent URI parameters to resources
+function _addRaml2htmlProperties(ramlObj, parentUrl, allUriParameters) {
+  // Add unique id's to top level documentation chapters
+  if (ramlObj.documentation) {
+    ramlObj.documentation.forEach((docSection) => {
+      docSection.uniqueId = _makeUniqueId(docSection.title);
+    });
+  }
+
+  if (!ramlObj.resources) {
+    return ramlObj;
+  }
+
+  ramlObj.resources.forEach((resource) => {
+    resource.parentUrl = parentUrl || '';
+    resource.uniqueId = _makeUniqueId(resource.parentUrl + resource.relativeUri);
+    resource.allUriParameters = [];
+
+    if (allUriParameters) {
+      resource.allUriParameters.push.apply(resource.allUriParameters, allUriParameters);
+    }
+
+    if (resource.uriParameters) {
+      resource.uriParameters.forEach((uriParameter) => {
+        resource.allUriParameters.push(uriParameter);
+      });
+    }
+
+    // Copy the RESOURCE uri parameters to the METHOD, because that's where they will be rendered.
+    if (resource.methods) {
+      resource.methods.forEach((method) => {
+        method.allUriParameters = resource.allUriParameters;
+      });
+    }
+
+    _addRaml2htmlProperties(resource, resource.parentUrl + resource.relativeUri, resource.allUriParameters);
+  });
+
+  return ramlObj;
+}
+
+// This uses the datatype-expansion library to expand all the root type to their canonical expanded form
+function _expandRootTypes(types) {
+  if (!types) {
+    return types;
+  }
+
+  Object.keys(types).forEach((key) => {
+    tools.expandedForm(types[key], types, (err, expanded) => {
+      if (expanded) {
+        tools.canonicalForm(expanded, (err2, canonical) => {
+          if (canonical) {
+            types[key] = canonical;
+          }
+        });
+      }
+    });
+  });
+
+  return types;
+}
+
+function _enhanceRamlObj(ramlObj) {
+  ramlObj = ramlArrayToObject(ramlObj);
+  const types = makeConsistent(_expandRootTypes(ramlObj.types));
+  delete ramlObj.types;
+  ramlObj = makeConsistent(ramlObj, types);
+  ramlObj = recursiveObjectToArray(ramlObj);
+  ramlObj = _addRaml2htmlProperties(ramlObj);
+
+  if (types) {
+    ramlObj.types = types;
+  }
+
+  return ramlObj;
+}
+
+function _sourceToRamlObj(source) {
+  if (typeof source === 'string') {
+    if (fs.existsSync(source) || source.indexOf('http') === 0) {
+      // Parse as file or url
+      return raml.loadApi(source, { rejectOnErrors: false }).then((result) => {
+        if (result._node._universe._typedVersion === '0.8') {
+          throw new Error('_sourceToRamlObj: only RAML 1.0 is supported!');
+        }
+
+        if (result.expand) {
+          return result.expand(true).toJSON({ serializeMetadata: false });
+        }
+
+        return new Promise((resolve, reject) => {
+          reject(new Error('_sourceToRamlObj: source could not be parsed. Is it a root RAML file?'));
+        });
+      });
+    }
+
+    return new Promise((resolve, reject) => {
+      reject(new Error('_sourceToRamlObj: source does not exist.'));
+    });
+  } if (typeof source === 'object') {
+    // Parse RAML object directly
+    return new Promise((resolve) => {
+      resolve(source);
+    });
+  }
+
+  return new Promise((resolve, reject) => {
+    reject(new Error('_sourceToRamlObj: You must supply either file, url or object as source.'));
+  });
+}
+
+const parse = (source) => {
+  return _sourceToRamlObj(source).then(ramlObj => {
+    return _enhanceRamlObj(ramlObj)
+  });
+}
+
+module.exports = {
+  parse,
+}

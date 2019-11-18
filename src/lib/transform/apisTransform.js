0050a800b8b0f2b4f66a5dcf3066c2b50784d6e6
Mu Qin <magenta2127@gmail.com>
2019-11-18 19:30:35 +0800

    format matched raml page

--- src/lib/transform/apisTransform.js
+++ src/lib/transform/apisTransform.js
@@ -4,20 +4,43 @@ import ramlFormatter from '@src/lib/parse/ramlFormatter'
 
 const ramlFilePath = path.resolve(__dirname, '../../../docs/api.raml')
 
-const getApiContent = (apis, ramlPages) => {
+const formatRequestHeaders = (resource, securitySchemes) => {
+  const { headers, securedBy } = resource
+  console.log(securedBy)
+}
+
+const formatRamlPage = (page, securitySchemes) => {
+  const formattedPage = {
+    title: page.displayName,
+    request_url: page.resource.relativeUri,
+    method: page.method,
+    request_headers: [],
+    params: [],
+    responses: [],
+    response_headers: [],
+  }
+  const { resource } = page;
+  // console.log(resource)
+  const requestHeaders = formatRequestHeaders(resource, securitySchemes)
+
+  return formattedPage;
+}
+
+const getApiContent = (apis, ramlPages, securitySchemes) => {
   apis.forEach(api => {
     const { method, relativeUrl } = api;
     const matchedRamlPage = ramlPages.find(ramlPage => {
       return ramlPage.resource.method === method && ramlPage.resource.relativeUri === relativeUrl;
     })
-    console.log(matchedRamlPage)
+    formatRamlPage(matchedRamlPage, securitySchemes)
   })
 }
 
-ramlParser.parse(ramlFilePath).then(resp => {
-  const ramlPages = ramlFormatter.getPages(resp)
+ramlParser.parse(ramlFilePath).then(parsedResult => {
+  console.log(parsedResult.securitySchemes)
+  const ramlPages = ramlFormatter.getPages(parsedResult)
   const apis = [
     { method: 'get', relativeUrl: '/document/token' },
   ]
-  getApiContent(apis, ramlPages)
+  getApiContent(apis, ramlPages, parsedResult.securitySchemes)
 }).catch(err => console.log('RAML ERR', err))
